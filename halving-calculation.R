38c3858145ce63a6eaa90b09b449e7b6c29db49e
maxxxzdn <50237680+maxxxzdn@users.noreply.github.com>
2020-10-07 10:56:54 +0200

    shift function added
    shift function allows handling negative values properly

--- halving-calculation.R
+++ halving-calculation.R
@@ -24,13 +24,38 @@ halving_time  <- function(x1,x2,y1,y2){
     -(x2-x1)/(y2-y1)*log10(2)
 } 
 
+# shifts negative time points 
+# If TIME >= 0: no shift of time points 
+# If TIME < 0: If they are in the month before treatment start (i.e. -1<=x<0), sets first negative time points to 0 (without changing the other time points). 
+# All negative time points that are below one month (TIME<-1) are neglected at all.
+# input: data for single patient
+# output: data with negative values either shifted or neglected
+shift  <- function(df){
+    
+    zero_point  <- which(df$TIME-0==min(df$TIME-0)) #search for the positive time point closest to 0
+    if (df$TIME[zero_point] < 0){ # if the point is negative 
+        zero_point  <- zero_point + 1 # take the next point to the right (surely positive)
+    }
+    if (zero_point==1){ # if the first point is positive no need to consider negative values
+        return(df)
+    }
+    
+    else if(df[zero_point-1,'TIME']<0){ # if there is a negative point
+        if (df[zero_point-1,'TIME']>-1 & !(df[zero_point,'TIME'] == 0)){ # if there is no 0 value already and the negative point is closer to 0 than a month -> shift negative to 0
+            df[zero_point-1,'TIME']  <- 0
+            return(df)
+        }
+        else {return(df[zero_point:nrow(df),])} # omit remaining negative points
+    }    
+}
+
 # list to include all patients with sufficient data 
 good_patients  <- c()
 
 for(patient in unique(data$PatID)){
     pat  <- subset(data[1:3], PatID == patient) # subset for a single patient with lQL column excluded
     pat  <- na.omit(pat) # omit rows with NA in LRATIO or TIME
-    pat$TIME <- pat$TIME - min(pat$TIME) # shift TIME variable to zero if there are negative values for it
+    pat  <- shift(pat) # shift TIME variable to zero if needed
     if (nrow(pat) < 3){next}
     cond1  <- nrow(subset(pat, TIME >= 0 & TIME < 2)) > 0 # at least 1 point with TIME between 0 and 2 
     cond2  <- nrow(subset(pat, TIME >=2 & TIME < 7)) > 0  # at least 1 point with TIME between 2 and 7
